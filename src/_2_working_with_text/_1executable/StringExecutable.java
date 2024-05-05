c85274eced76d063ad37cc2eebbe467635b89e49
Sanjay Kumar S <s.sanjaykumar1@zohomail.in>
2024-05-05 16:07:59 +0530

    Understand string and its method

--- src/_2_working_with_text/_1executable/StringExecutable.java
+++ src/_2_working_with_text/_1executable/StringExecutable.java
@@ -10,12 +10,146 @@ public class StringExecutable {
         understandStringWhitespaceRemoval();
         understandStringCharAt();
         understandStringCompareTo();
+        understandStringContains();
+        understandStringConcat();
+        understandStringSubString();
+        understandStringIndexOf();
+        understandStringSplit();
+    }
+
+    private static void understandStringSplit() {
+        System.out.println("-- UNDERSTAND STRING SPLIT --");
+
+        String fruits = "Apple,Banana,Guava,JackFruit";
+        // Regex can be complex or straight forward as a simple string literal
+        // split(regex)
+        // split(regex, noOfOutputElementsInTheArray)
+        String[] unlimitedFruitsArray = fruits.split(",");
+        for(String fruit : unlimitedFruitsArray) {
+            System.out.println(fruit);
+        }
+        System.out.println("---");
+        String[] limitedFruitsArray = fruits.split(",", 2);
+        for(String fruit : limitedFruitsArray) {
+            System.out.println(fruit);
+        }
+    }
+
+    private static void understandStringIndexOf() {
+        System.out.println("-- UNDERSTAND STRING INDEX OF --");
+
+        // indexOf() - tells us where inside the string a substring exists
+        // indexOf(string)
+        // indexOf(string, fromIndex)
+        // indexOf(int characterUnicode)
+        // indexOf(int characterUnicode, fromIndex)
+        String statement = "Four score and seven years later";
+        // Returns the start index of string mentioned if present else returns -1
+        System.out.println(statement.indexOf("score"));
+
+        // Split the string into three parts (Area code (234), Exchange(222), Line number(3331))
+        String phoneNumber = "(234) 222-3331";
+        String areaCode = parseCode(phoneNumber, PhoneCode.AREA_CODE);
+        String exchangeCode = parseCode(phoneNumber, PhoneCode.EXCHANGE_CODE);
+        String lineNumber = parseCode(phoneNumber, PhoneCode.LINE_CODE);
+        System.out.println(areaCode);
+        System.out.println(exchangeCode);
+        System.out.println(lineNumber);
+    }
+
+    private static String parseCode(String phoneNumber, PhoneCode phoneCode) {
+        // Switch Expression
+        // Returns a value directly
+        return switch (phoneCode) {
+            case AREA_CODE -> phoneNumber.substring(phoneNumber.indexOf("(") + 1, phoneNumber.indexOf(")"));
+            case EXCHANGE_CODE -> phoneNumber.substring(phoneNumber.indexOf(" ") + 1, phoneNumber.indexOf("-"));
+            case LINE_CODE -> phoneNumber.substring(phoneNumber.indexOf("-") + 1);
+        };
+    }
+
+    private static void understandStringSubString() {
+        System.out.println("-- UNDERSTAND STRING SUB STRING --");
+        String fruit = "apple";
+        // Uppercase the first letter alone
+        // subString() - overloaded method
+        // subString(startIndexInclusive)
+        // subString(startIndexInclusive, endIndexExclusive)
+        String casedFruit = fruit.substring(0, 1).toUpperCase() + fruit.substring(1);
+        System.out.println(casedFruit);
+    }
+
+    private static void understandStringConcat() {
+        System.out.println("-- UNDERSTAND STRING CONCAT --");
+        String firstName = "John";
+        String lastName = "Smith";
+        String fullName;
+
+        // Concat using + operator
+        fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+
+        // Concat using concat()
+        fullName = firstName.concat(" ").concat(lastName);
+        System.out.println(fullName);
+
+        // Concat using format()
+        fullName = String.format("%s %s", firstName, lastName);
+        System.out.println(fullName);
+
+        // If we are concatenating two or three strings then the above three approaches are fine
+        // But if are concatenating in a loop or so, then for each concatenation java creates a new string that
+        // populates the heap space memory, which is not recommended
+
+        //Concat using StringBuilder - Newer Version
+
+        // In this method new Strings are not formed - the appended string is maintained as a byte array and when
+        // toString() is called, its converted to String and returned to us
+        // Not thread safe
+        // Faster
+        fullName = new StringBuilder(firstName)
+                .append(" ")
+                .append(lastName)
+                .toString();
+        System.out.println(fullName);
+
+        // Concat using StringBuffer - Newer Version
+
+        // In this method new Strings are not formed - the appended string is maintained as a byte array and when
+        // toString() is called, its converted to String and returned to us
+        // Thread safe
+        // Slower
+        fullName = new StringBuffer(firstName)
+                .append(" ")
+                .append(lastName)
+                .toString();
+        System.out.println(fullName);
+    }
+
+    private static void understandStringContains() {
+        System.out.println("-- UNDERSTAND STRING CONTAINS --");
+        String statement = "Four score and seven years later";
+        System.out.println(statement.contains("score"));
+        System.out.println(statement.contains("score "));
+        System.out.println(statement.contains("score  "));
     }
 
     private static void understandStringCompareTo() {
+        System.out.println("-- UNDERSTAND STRING COMPARE TO --");
+        String firstWord = "Apple";
+        String secondWord = "Banana";
+
+        //compareTo()
+        //instance1.compareTo(instance2)
+        //Returns -ve if instance1 comes before instance2 alphabetically
+        //Returns 0 if instance1 and instance2 are equal
+        //Returns +ve if instance2 comes before instance1 alphabetically
+        System.out.println(firstWord.compareTo(secondWord));
+        //Converts all to Uppercase before comparing
+        System.out.println(firstWord.compareToIgnoreCase(secondWord));
     }
 
     private static void understandStringCharAt() {
+        System.out.println("-- UNDERSTAND STRING CHAR AT --");
         String name = "Micheal";
         //0 based index
         //Can throw StringIndexOutOfBoundsException
@@ -23,6 +157,7 @@ public class StringExecutable {
     }
 
     private static void understandStringWhitespaceRemoval() {
+        System.out.println("-- UNDERSTAND STRING WHITESPACE REMOVAL --");
         String firstName = " Jake ";
         System.out.printf("The first name of the user is:'%s'%n", firstName);
 
@@ -42,6 +177,7 @@ public class StringExecutable {
     }
 
     private static void understandStringReplace() {
+        System.out.println("-- UNDERSTAND STRING REPLACE --");
         String replaceableString = "Hello, how are you?";
         //Replace a character
         System.out.println(replaceableString.replace('h', 'H'));
@@ -50,6 +186,7 @@ public class StringExecutable {
     }
 
     private static void understandStringBlankAndEmpty() {
+        System.out.println("-- UNDERSTAND STRING BLANK AND EMPTY --");
         String emptyString = "";
         String blankString = " ";
         //isEmpty() simply tells if the length of the string is zero
@@ -61,14 +198,16 @@ public class StringExecutable {
     }
 
     private static void understandStringCasing() {
+        System.out.println("-- UNDERSTAND STRING CASING --");
         String countryCode = "Ind";
         System.out.println(countryCode.toUpperCase());
-        
+
         String name = "PRAthIk";
         System.out.println(name.toLowerCase());
     }
 
     private static void understandStringCreation() {
+        System.out.println("-- UNDERSTAND STRING CREATION --");
         String fruit = "apple";
         String anotherFruit = "apple";
 
@@ -81,5 +220,11 @@ public class StringExecutable {
 
         //While using new keyword we are forcing java to create a new address for each literal we pass
         System.out.println(vegetable == anotherVegetable);
+    }
+
+    private enum PhoneCode {
+        AREA_CODE,
+        EXCHANGE_CODE,
+        LINE_CODE
     }
 }
